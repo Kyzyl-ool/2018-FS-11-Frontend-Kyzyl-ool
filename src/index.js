02460ef243cad8f49b05702fcce83121a7b44c5d
Kyzyl-ool <kyzyloolk@mail.ru>
2018-10-11 15:58:30 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import set_background_color from "./lib/test.js";
 import print_message from "./lib/print.js";
-const TEXT = "Hello, guys!"
+const TEXT = "Hello, guys!";
 
 
-set_background_color('#AA0000');
 print_message(TEXT);
--- src/lib/print.js
+++ src/lib/print.js
@@ -1,5 +1,9 @@
+import set_background_color from "./test";
+
 function print_message(text) {
+    set_background_color("red");
     document.write(text);
+
 }
 
 export default print_message;
