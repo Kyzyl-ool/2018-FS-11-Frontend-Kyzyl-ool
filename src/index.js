47a4b58ca708adb35cad3425ece24c8246cc7d10
Kyzyl-ool <kyzyloolk@mail.ru>
2018-10-13 01:18:21 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import print_message from "./lib/print.js";
-const TEXT = "Hello, guys!";
-
+const TEXT = "Hello, Martin! You are the best contributor!";
 
 print_message(TEXT);
--- src/lib/print.js
+++ src/lib/print.js
@@ -1,17 +1,6 @@
-import set_background_color from "./test";
-
 function print_message(text) {
-    var div = document.createElement("div");
-    div.innerHTML = text;
-    div.style.height = "20px";
-    div.style.width = "200px";
-    div.style.border = "5px double black";
-    div.style.backgroundColor = "red";
-    div.align = "center";
-
-
-    document.body.appendChild(div);
-
+    var aaaa = document.querySelector(".Krasnyi_kvadrat");
+    aaaa.innerHTML = text;
 }
 
 export default print_message;
