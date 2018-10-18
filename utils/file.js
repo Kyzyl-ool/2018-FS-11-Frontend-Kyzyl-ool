85ab2ef7faa27d0a563e539eb3aec9ee059c5d15
Kyzyl-ool <kyzyloolk@mail.ru>
2018-10-18 15:51:33 +0300

    consts added

--- utils/file.js
+++ utils/file.js
@@ -1,19 +1,18 @@
+const KB = 2 ** 10;
+const MB = 2 ** 20;
+const GB = 2 ** 30;
+
 function getReadableSize(size) {
-    if (size >= 2 ** 10) {
-        if (size >= 2 ** 20) {
-            if (size >= 2 ** 30) {
-                if (size >= 2 ** 40) {
-                    return `${(size / 2 ** 30).toFixed(2)} Гбайт`;
-                }
-            } else {
-                return `${(size / 2 ** 20).toFixed(2)} Мбайт`;
+    if (size >= KB) {
+        if (size >= MB) {
+            if (size >= GB) {
+                return `${(size / 2 ** 30).toFixed(2)} Гбайт`;
             }
-        } else {
-            return `${(size / 2 ** 10).toFixed(2)} Кбайт`;
+            return `${(size / 2 ** 20).toFixed(2)} Мбайт`;
         }
-    } else {
-        return `${size} байт`;
+        return `${(size / 2 ** 10).toFixed(2)} Кбайт`;
     }
+    return `${size} байт`;
 }
 
-alert(getReadableSize(2 ** 10 + 315));
+alert(getReadableSize(100000));
