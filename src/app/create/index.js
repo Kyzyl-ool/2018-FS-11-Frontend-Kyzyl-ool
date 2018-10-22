aef4cb11a13f3534366aa41f09502c7e4b020721
Kyzyl-ool <kyzyloolk@mail.ru>
2018-10-22 12:29:15 +0300

    button added

--- src/app/create/index.js
+++ src/app/create/index.js
@@ -5,7 +5,7 @@ import shadowBodystyle from './shadow.css';
 window.document.body.innerHTML = `
 <style>${shadowBodystyle.toString()}</style>
 
-<message-form></message-form>
-<button> Отправить </button>
-
+<message-form> </message-form>
+<button class="SendButton">Отправить</button>
 `;
+window.document.body.className = 'flex';
