6d100f22d7dcabc228ad17bc04c2026a8fe78bf2
Kyzyl-ool <kyzyloolk@mail.ru>
2018-11-06 15:09:34 +0300

    It works НОРМАЛЬНО

--- src/lib/components/message-form/MessageForm.js
+++ src/lib/components/message-form/MessageForm.js
@@ -4,14 +4,30 @@ import  './MessageForm.css';
 class MessageForm extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            text: ''
+        };
+    }
+
+    disp(event) {
+        event.preventDefault();
+      this.props.dispatcher(this.state.text);
+      this.setState({text: ''});
+    }
+
+
+
+    updateData(event) {
+        this.setState({text: event.target.value});
     }
 
     render() {
         return (
-            <div className="MessageForm">
-                <input className="InputForm" type="text" placeholder="Enter your message..."/>
-                <input className="SendButton" type="submit" value="Send" />
-            </div>
+              <form className="MessageForm" onSubmit={this.disp.bind(this)}>
+              <input  value={this.state.text} onChange={this.updateData.bind(this)} className="InputForm" type="text" placeholder="Enter your message..."/>
+                <input onClick={this.disp.bind(this)} className="SendButton" type="submit" value="Send" />
+              </form>
         );
     }
 }
